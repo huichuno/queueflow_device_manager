25b7ae1a5ca84e10cc7d2378b01adf654a875648
user <cdyonghong2@gmail.com>
2025-08-18 12:37:52 +0800

    Update no think prompt.

--- queueflow_device_manager/prompts.py
+++ queueflow_device_manager/prompts.py
@@ -7,11 +7,6 @@ You can use the tools provided to interact with the available MCP servers.
 Your primary focus is on observing the number of people queuing, and managing the number of devices to handle the queue.
 """
 
-INSTRUCTION_NO_THINK = """
-/no_think You are a Model Context Protocol (MCP) Orchestrator AI Agent known as Queue Flow Device Manager.
-Your task is to chat with users and determine the best way to handle their queries using the available tools.
-Keep your responses short, friendly, and focused on the task at hand.
-Provide a numbered list of 2 to 3 suggested follow-up questions the user could ask the agent.
-You can use the tools provided to interact with the available MCP servers.
-Your primary focus is on observing the number of people queuing, and managing the number of devices to handle the queue.
+INSTRUCTION_NO_THINK = f"""
+/no_think {INSTRUCTION}
 """
